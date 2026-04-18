30903b5f4e122b64ec4d66b8d7d53ea08ca38adb
Firas Amara <firas.amara@tbs.u-tunis.tn>
2026-04-18 17:58:14 +0100

    Initial Commit

---
+++ JSSwiftBridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JSSwiftBridge
+//
+//  Created by Hold Apps on 18/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JSSwiftBridge/JSSwiftBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSSwiftBridgeApp.swift
+//  JSSwiftBridge
+//
+//  Created by Hold Apps on 18/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct JSSwiftBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
